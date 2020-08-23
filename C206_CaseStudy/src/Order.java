cf008fddce9cdb635a167ae24e7bb0e00e4dac1d
19012079 <19012079@MingHao-MacBookPro>
2020-08-24 02:52:36 +0800

    Completed Order Class -Ming Hao

--- C206_CaseStudy/src/Order.java
+++ C206_CaseStudy/src/Order.java
@@ -1,4 +1,44 @@
+import java.util.ArrayList;
 
 public class Order {
 
+	private String studentId;
+	private String orderDate;
+	private ArrayList<MenuItem> items;
+	
+	public Order(String studentId, String orderDate, ArrayList<MenuItem> items) {
+		this.studentId = studentId;
+		this.orderDate = orderDate;
+		this.items = items;
+	}
+
+	public String getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(String studentId) {
+		this.studentId = studentId;
+	}
+
+	public String getOrderDate() {
+		return orderDate;
+	}
+
+	public void setOrderDate(String orderDate) {
+		this.orderDate = orderDate;
+	}
+
+	public ArrayList<MenuItem> getItems() {
+		return items;
+	}
+
+	public void setItems(ArrayList<MenuItem> items) {
+		this.items = items;
+	}
+	
+	public String toString() {
+		return toString();
+	}
+	
+	
 }
