b3c8712446e26e0a817f307ac3f60723c96461a7
19046694 <19046694@192.168.0.101>
2020-08-24 01:55:04 +0800

    MenuItem  completed - mika

--- C206_CaseStudy/src/MenuItem.java
+++ C206_CaseStudy/src/MenuItem.java
@@ -1,4 +1,52 @@
 
 public class MenuItem {
 
+	private String category;
+	private String name;
+	private boolean healthyChoice;
+	private double price;
+
+	public MenuItem(String category, String name, boolean healthyChoice, double price) {
+		this.category = category;
+		this.healthyChoice = healthyChoice;
+		this.name = name;
+		this.price = price;
+
+	}
+
+	public String getcategory() {
+		return category;
+	}
+
+	public void setcategory(String category) {
+		this.category = category;
+	}
+
+	public boolean ishealthyChoice() {
+		return healthyChoice;
+	}
+
+	public void sethealthyChoice(boolean healthyChoice) {
+		this.healthyChoice = healthyChoice;
+	}
+
+	public String getname() {
+		return name;
+	}
+
+	public void setname(String name) {
+		this.name = name;
+	}
+
+	public String getprice() {
+		return name;
+	}
+
+	public void getprice(double price) {
+		this.price = price;
+	}
+
+	public String toString() {
+		return toString();
+	}
 }
