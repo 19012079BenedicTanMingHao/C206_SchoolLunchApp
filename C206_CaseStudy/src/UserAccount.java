03e91a6bd765e4a199be2c0c664405f3b5fc61db
19022507 <19022507@DESKTOP-KPJ4620>
2020-08-24 13:46:15 +0800

    update

---
+++ C206_CaseStudy/src/UserAccount.java
@@ -0,0 +1,15 @@
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 19022507, 24 Aug 2020 12:41:45 pm
+ */
+
+/**
+ * @author 19022507
+ *
+ */
+public class UserAccount {
+
+}
