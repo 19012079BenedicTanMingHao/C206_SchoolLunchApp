5a24c856133e536ffae4234458995238f1ec73be
19022507 <19022507@DESKTOP-KPJ4620>
2020-08-24 15:01:25 +0800

    update account for new requirements

--- C206_CaseStudy/src/UserAccount.java
+++ C206_CaseStudy/src/UserAccount.java
@@ -5,6 +5,34 @@ public class UserAccount {
 	private String accountName;
 	private String password;
 	private int contactNo;
+	private ArrayList<UserAccount> ParentList;
+	private ArrayList<UserAccount> StudentList;
+	private int studentID;
+	private int creditBal;
+	private String accountType; //student or parent
+
+	public void ParentAccount(String accountName, int contactNo, String password, String accountType, int creditBal) {//creating parent account
+		this.accountName = accountName;
+		this.contactNo = contactNo;
+		this.accountType = accountType;
+		this.password = password;
+		this.setCreditBal(creditBal);
+	}
+	public void StudentAccount(String accountName, int contactNo, String password, String accountType, int studentID) {//creating student account
+		this.accountName = accountName;
+		this.accountType = accountType;
+		this.contactNo = contactNo;
+		this.password = password;
+		this.setStudentID(studentID);
+	}
+
+	public String getAccountType() {
+		return accountType;
+	}
+
+	public void setAccountType(String accountType) {
+		this.accountType = accountType;
+	}
 
 	public int getContactNo() {
 		return contactNo;
@@ -12,13 +40,6 @@ public class UserAccount {
 
 	public void setContactNo(int contactNo) {
 		this.contactNo = contactNo;
-	}
-
-	private ArrayList<UserAccount> accountList;
-	
-	public UserAccount(String accountName, String password) {
-		this.accountName = accountName;
-		this.password = password;
 	}
 
 	public String getAccountName() {
@@ -37,13 +58,43 @@ public class UserAccount {
 		this.password = password;
 	}
 
-	public ArrayList<UserAccount> getAccountList() {
-		return accountList;
+	public void updateAccount(String accountName, int contactNo) {
+		this.accountName = accountName;
+		this.contactNo = contactNo;
 	}
 
-	public void setAccountList(ArrayList<UserAccount> accountList) {
-		this.accountList = accountList;
+	public int getStudentID() {
+		return studentID;
+	}
+
+	public void setStudentID(int studentID) {
+		this.studentID = studentID;
+	}
+
+	public int getCreditBal() {
+		return creditBal;
+	}
+
+	public void setCreditBal(int creditBal) {
+		this.creditBal = creditBal;
+	}
+	public void getStudentAccount(int studentID) {
+		this.studentID = studentID;
+	}
+
+	public ArrayList<UserAccount> getStudentList() {
+		return StudentList;
+	}
+
+	public void setStudentList(ArrayList<UserAccount> studentList) {
+		StudentList = studentList;
+	}
+
+	public ArrayList<UserAccount> getParentList() {
+		return ParentList;
+	}
+	public void setParentList(ArrayList<UserAccount> parentList) {
+		ParentList = parentList;
 	}
 	
-
 }
